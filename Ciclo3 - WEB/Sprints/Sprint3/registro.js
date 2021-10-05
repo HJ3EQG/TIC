026cbbc285d7341e428cd49f3881ab0f09a60373
Juan M. Arango <juanmaracho10@gmail.com>
2021-10-04 23:36:32 -0500

    add sprint 1

---
+++ Ciclo3 - WEB/Sprints/Sprint3/registro.js
@@ -0,0 +1,20 @@
+// Declaracion del arreglo global de objetos
+let arregloGlobal = [];
+
+function agregarRegistro();
+
+function obtenerRegPasaporte(arreglo);
+
+function ordenarArreglo(arreglo);
+
+function filtrarCorreo(arreglo);
+
+
+
+// Exportacion de funciones necesarios
+module.exports = {
+    agregarRegistro,
+    obtenerRegPasaporte,
+    ordenarArreglo,
+    filtrarCorreo,
+}
